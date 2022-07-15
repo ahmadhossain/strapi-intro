2582f0606e76a44df2d593a007e6eb0a289a8dc6
Ahmad Hossain <ahmadhossain.bu@gmail.com>
2022-07-15 19:54:56 +0600

    couldinary provider added

--- config/plugins.js
+++ config/plugins.js
@@ -1,20 +1,21 @@
 // path: ./config/plugins.js
 
 module.exports = ({ env }) => ({
-    // ...
-    upload: {
-      config: {
-        provider: 'aws-s3', // For community providers pass the full package name (e.g. provider: 'strapi-provider-upload-google-cloud-storage')
-        providerOptions: {
-          accessKeyId: env('AWS_ACCESS_KEY_ID'),
-          secretAccessKey: env('AWS_ACCESS_SECRET'),
-          region: env('AWS_REGION'),
-          params: {
-            Bucket: env('AWS_BUCKET'),
-          },
-        },
+  // ...
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
-    // ...
-  });
-  
+  },
+  // ...
+});
